95d004dd38a8f43243209691e8b7e821dd9b7b31
Mark Hahl <mark.hahl@sigaint.au>
2024-11-07 22:58:32 +1100

    fix typos in urls

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 pkg install py311-ansible-core 
 
 cat > /etc/cron.d/baseline <<EOF
-*/30 * * * * root /usr/local/bin/ansible-pull https://git.sr.ht/~mhahl/sigaint-freebsd-baseline baseline.yaml --clean
+*/30 * * * * root /usr/local/bin/ansible-pull -U https://git.sr.ht/~mhahl/sigaint-freebsd-baseline baseline.yaml --clean
 EOF
