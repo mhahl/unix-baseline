24b7089f9059604ac7ae17f51c3a5ce327165bc4
Mark Hahl <mark.hahl@sigaint.au>
2024-11-07 23:06:29 +1100

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pkg install py311-ansible-core 
+pkg install py311-ansible py311-ansible-core 
 
 cat > /etc/cron.d/baseline <<EOF
 */30 * * * * root /usr/local/bin/ansible-pull -U https://git.sr.ht/~mhahl/sigaint-freebsd-baseline baseline.yaml --clean
