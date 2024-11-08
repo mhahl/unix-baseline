882e41d82e1b169e845bc4282d5df1fa0eba95c0
Mark Hahl <mark.hahl@sigaint.au>
2024-11-08 14:52:31 +1100

    encrypt passwords

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+echo -n "Enter Password:"
+
+read password
+
+mkdir -p /var/db/baseline
+
+echo "$password" > /var/db/baseline/.credentials
+chmod 0600 /var/db/baseline/.credentials
+
 pkg install py311-ansible py311-ansible-core 
 
 cat > /etc/cron.d/baseline <<EOF
-*/30 * * * * root /usr/local/bin/ansible-pull -U https://git.sr.ht/~mhahl/sigaint-freebsd-baseline baseline.yaml --clean
+*/30 * * * * root /usr/local/bin/ansible-pull -U https://git.sr.ht/~mhahl/sigaint-freebsd-baseline baseline.yaml --clean --vault-pass-file /var/db/baseline/.credentials
 EOF
