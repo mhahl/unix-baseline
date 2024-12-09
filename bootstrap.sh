912631f74bcc419d8138aa423cde338ff21b8690
Mark Hahl <mark.hahl@sigaint.au>
2024-12-09 11:24:31 +1100

    refactorls

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ touch /usr/local/etc/baseline/config.yaml
 echo "$password" > /var/db/baseline/.credentials
 chmod 0600 /var/db/baseline/.credentials
 
-pkg install py311-ansible py311-ansible-core 
+pkg install py311-ansible py311-ansible-core
 
 cat > /etc/cron.d/baseline <<EOF
 */30 * * * * root /usr/local/bin/ansible-pull -U https://git.sr.ht/~mhahl/sigaint-freebsd-baseline baseline.yaml --clean --vault-pass-file /var/db/baseline/.credentials >/dev/null 2>&1
@@ -19,3 +19,6 @@ EOF
 
 echo "Applying baseline"
 ansible-pull -U https://git.sr.ht/~mhahl/sigaint-freebsd-baseline baseline.yaml --clean --vault-pass-file /var/db/baseline/.credentials
+
+
+
