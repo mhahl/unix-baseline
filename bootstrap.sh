728db10609df7107c6ef184952ee4a6f5475eb28
Mark Hahl <mark.hahl@sigaint.au>
2025-02-28 12:02:23 +1100

    update bootstrap script to detect linux

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+platform='unknown'
+unamestr=$(uname)
+if [ "$unamestr" = 'Linux' ]; then
+   platform='linux'
+elif [ "$unamestr" = 'FreeBSD' ]; then
+   platform='freebsd'
+fi
+
 echo -n "Vault Password: "
 read password
 
@@ -11,13 +19,23 @@ touch /usr/local/etc/baseline/config.yaml
 echo "$password" > /var/db/baseline/.credentials
 chmod 0600 /var/db/baseline/.credentials
 
-pkg install py311-ansible py311-ansible-core
+if [ "$platform" = 'linux' ]; then
+  dnf install -y ansible
 
-cat > /etc/cron.d/baseline <<EOF
-*/30 * * * * root /usr/local/bin/ansible-pull -U https://git.sr.ht/~mhahl/sigaint-sigaint-soe baseline.yaml --clean --vault-pass-file /var/db/baseline/.credentials >/dev/null 2>&1
-EOF
+  cat > /etc/cron.d/baseline <<EOF
+  */30 * * * * root /usr/bin/ansible-pull -U https://github.com/mhahl/freebsd-baseline baseline.yaml --clean --vault-pass-file /var/db/baseline/.credentials >/dev/null 2>&1
+  EOF
+
+elif [ "$platform" = 'freebsd' ]; then
+  pkg install py311-ansible py311-ansible-core
+
+  cat > /etc/cron.d/baseline <<EOF
+  */30 * * * * root /usr/local/bin/ansible-pull -U https://github.com/mhahl/freebsd-baseline baseline.yaml --clean --vault-pass-file /var/db/baseline/.credentials >/dev/null 2>&1
+  EOF
+
+fi
 
 echo "Applying baseline"
-ansible-pull -U https://git.sr.ht/~mhahl/sigaint-sigaint-soe baseline.yaml --clean --vault-pass-file /var/db/baseline/.credentials
+ansible-pull -U https://github.com/mhahl/freebsd-baseline --clean --vault-pass-file /var/db/baseline/.credentials
 
 
