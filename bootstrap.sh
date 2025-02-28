21662a5b57fad121ca680dda0d1998ff3b7ea443
Mark Hahl <mark.hahl@sigaint.au>
2025-02-28 11:26:38 +1100

    add fedora support
    add support for fedora which includes configuration of chrony, systemd-resolv and authselect for sssd.

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,10 +14,10 @@ chmod 0600 /var/db/baseline/.credentials
 pkg install py311-ansible py311-ansible-core
 
 cat > /etc/cron.d/baseline <<EOF
-*/30 * * * * root /usr/local/bin/ansible-pull -U https://git.sr.ht/~mhahl/sigaint-freebsd-baseline baseline.yaml --clean --vault-pass-file /var/db/baseline/.credentials >/dev/null 2>&1
+*/30 * * * * root /usr/local/bin/ansible-pull -U https://git.sr.ht/~mhahl/sigaint-sigaint-soe baseline.yaml --clean --vault-pass-file /var/db/baseline/.credentials >/dev/null 2>&1
 EOF
 
 echo "Applying baseline"
-ansible-pull -U https://git.sr.ht/~mhahl/sigaint-freebsd-baseline baseline.yaml --clean --vault-pass-file /var/db/baseline/.credentials
+ansible-pull -U https://git.sr.ht/~mhahl/sigaint-sigaint-soe baseline.yaml --clean --vault-pass-file /var/db/baseline/.credentials
 
 
